64e5431132e1635f11d3d7692679af4318227fef
Ashish <ashishredhu5810@gmail.com>
2023-08-10 22:55:37 +0530

    Remove button functionality added

--- home.js
+++ home.js
@@ -1,5 +1,6 @@
 // JS says whenever you declare a variable inside a class, you have to initialize it at the same time.
 
+// The objects of data class are the tasks that we are adding, deleting, editing.
 class data{
     constructor(heading,details,time)
     {
@@ -8,80 +9,158 @@ class data{
         this.time=time;
     }    
 }
-
-let arr=[];
-// let add=document.getElementById("button1");
-// let workcontainer=document.getElementById("workcontainer");
-// add.onclick=adding;
-// let clickcount=0;
-// function adding(){
-//     clickcount++;
-//     let head=prompt("Enter the heading: ");
-//     let det=prompt("Enter the details: ");
-//     let tim=prompt("Time on which you want to do the work: ");
-//     let ob1=new data(head,det, tim);
-//     arr.push(ob1);
-
-// //    workcontainer.appendChild(`Heading=${ob1.heading} Details=${ob1.details} time=${ob1.time}`); This is not the right way to directly append in DOM. First you have to create an element then you can append it.
-    
-//    if((ob1.heading!='' && ob1.time!='') && (ob1.heading!=null && ob1.time!=null))
-//    {
-//    let p=document.createElement("p");
-//    p.textContent=`Heading=${ob1.heading} Details=${ob1.details} time=${ob1.time}  * ${clickcount} *`;
-//    workcontainer.appendChild(p);
-//    }
-   
-// }
- 
+let arr=[]; // This array will contain the objects of data class. 
+// ---------------------------------------------------------------------------------------
 
 
-// Add button ----------------------------------------------------------------------------------------------
+
+// Add button ----------------------------------------------------------------------------
 // Form open code.
-function openform(){
+function openform1(){
     document.querySelector('.overlay').classList.add('openoverlay');
-    document.querySelector('.form').classList.add('openform');
+    document.querySelector('.form1').classList.add('openform');
  }
 
 let add=document.getElementById("button1");
-add.addEventListener('click',openform);
-
+add.addEventListener('click',openform1);
 
 // Form close code.
-function closeform(){
+function closeform1(){
     document.querySelector('.overlay').classList.remove('openoverlay');
-    document.querySelector('.form').classList.remove('openform');
+    document.querySelector('.form1').classList.remove('openform');
 }
 let cross=document.getElementById("cross");
-cross.addEventListener('click', closeform);
-// ---------------------------------------------------------------------------------------------------------
+cross.addEventListener('click', closeform1);
+// ----------------------------------------------------------------------------------------
 
 
-// Add button of form
+
+// Add button of form --------------------------------------------------------------------
+function convertTime(timeInput) {
+    // const timeInput = document.getElementById("timing").value;
+    const timeParts = timeInput.split(":");
+    const hours24 = parseInt(timeParts[0]);
+    const minutes = timeParts[1];
+    
+    let hours12 = hours24 % 12;
+    if (hours12 === 0) {
+      hours12 = 12;
+    }
+    
+    const amPm = hours24 < 12 ? "AM" : "PM";
+    
+    const convertedTime = `${hours12}:${minutes} ${amPm}`;
+    return convertedTime;
+  }
 function addElements(){
-   // Show elements on screen.
+
+   // To show elements on screen.
     const h=document.getElementById('heading').value; 
     const d = document.getElementById('details').value;
-    const t = document.getElementById('timing').value;
+    const tt = document.getElementById('timing').value;
+    
+    const t=convertTime(tt);
+    
     const ob=new data(h,d,t);
     arr.push(ob);
-    let para=document.createElement('p');
-    // Both of the below will work.
-    // 1.) 
-    // para.textContent=`Heading=${ob.heading}, Details=${ob.details}, Time=${ob.time}`;
-    // 2.) 
-    para.textContent=`Heading=${arr[arr.length-1].heading}, Details=${arr[arr.length-1].details}, Time=${arr[arr.length-1].time}`;
-    document.getElementById("workcontainer").appendChild(para);
+    // The values entered in the form will come in h,d,t. Then we created an object and append the object in array. 
+     
+    let para1=document.createElement('p');
+    let para2=document.createElement('p');
+    let para3=document.createElement('p');
+     
+    para1.textContent=`Time: ${arr[arr.length-1].time}`;
+    para2.textContent=`Heading: ${arr[arr.length-1].heading}`;
+    para3.textContent=`Details: ${arr[arr.length-1].details}`;
+
+    let divtag=document.createElement('div');
+
+    divtag.appendChild(para1);
+    divtag.appendChild(para2);
+    divtag.appendChild(para3);
+     
+    document.getElementById("workcontainer").appendChild(divtag);
+    // Then we made 3-paragraphs that will contain the h,d,t values. Then we created one div that will contain all the 3-paras. At the end the div will pe added as a child of workcontain (big div).
     console.log("Ok done");
+
+    // Before storing anything in session/local storage we have to convert it into string.
     const userInputJSON=JSON.stringify(arr[arr.length-1]);
-    const keyInputJSON=JSON.stringify(arr[arr.length-1].heading);
-    localStorage.setItem(keyInputJSON,userInputJSON);
-    closeform();
+    const keyInputJSON=JSON.stringify(arr[arr.length-1].time);
+
+
+    // We are not using LocalStorage becuause it is possible that user will refresh, the page after adding some tasks. Then his tasks will be cleared from screen but available in Localstorage which has no meaning.
+    // localStorage.setItem(keyInputJSON,userInputJSON);
+
+    // That's why we are using sessionstorage. Valid upto a particular refresh/page change......
+    sessionStorage.setItem(keyInputJSON,userInputJSON);
+    // Storing the elements in session storage as well. Because it may happen that in future user may want to delete/edit some task. 
+    closeform1();
 }
 let addbutton=document.getElementById("addbuttonform");
 addbutton.addEventListener('click',addElements);
 
+// ---------------------------------------------------------------------------------------
 
 
+
+// Remove Button: -----------------------------------------------------------------------
+function removeElement(){
+    document.querySelector('.overlay').classList.add('openoverlay')
+    document.querySelector('.form2').classList.add('form2open');
+}
+let removebutton=document.getElementById("button3");
+removebutton.addEventListener('click', removeElement);
+
+// Cross icon of Remove form:
+function closeform2(){
+    document.querySelector('.overlay').classList.remove('openoverlay');
+    document.querySelector('.form2').classList.remove('form2open');
+}
+let cross2=document.getElementById("cross2");
+cross2.addEventListener('click',closeform2);
+
+// Remove button of Remove form:
+function removetask(){
+    let tt=document.getElementById("time").value;
+    let t=convertTime(tt);
+    let getelement=sessionStorage.getItem(JSON.stringify(t));
+    console.log(t);
+    console.log(getelement);
+    if(getelement!=null)
+    {
+        sessionStorage.removeItem(t); 
+
+        // Don't know why. But this below for loop is not letting code working fine.
+        // for(let i=0; i<arr.length; i++)
+        // {
+        //     if(arr[i].time==t)
+        //     {remove(arr[i]);}
+        // }
+        
+        // To select all the divs.
+        let divs=document.querySelectorAll('#workcontainer > div');
+        // Now iterating through each div.
+        for(let i=0; i<divs.length; i++)
+        {   
+            // Storing the div temporarily.
+            const tempdiv=divs[i];
+            // Selecting first para of each div in every iteration.
+            let para=divs[i].querySelector("p");
+                if(para!=null && para.textContent.includes(t))
+                {
+                   tempdiv.remove();
+                }
+        }
+
+        closeform2();
+        alert("Item successfully removed from taskbar.");
+        
+    }
+    else
+    {alert("Oops! The item is not present. Check Hours, Minutes, Am/Pm properly.");}
+}
+let removebuttonform=document.getElementById("removebuttonform");
+removebuttonform.addEventListener('click',removetask);
 
 
 
